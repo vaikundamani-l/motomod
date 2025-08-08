fbfe6cca504d0d1851a49bae65c6c2133dfebd79
vaikundamani-l <l.vaikundamani@gmail.com>
2025-08-08 11:24:02 +0530

    First commit for this project

---
+++ src/main/java/com/shop/motomod/MotoModApplication.java
@@ -0,0 +1,13 @@
+package com.shop.motomod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoModApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoModApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/motomod/MotoModApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.motomod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoModApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
